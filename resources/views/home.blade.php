9cb562f5373c4cd6f3bb9e761542fdbdc04c7172
hefterstome <iqbra127@gmail.com>
2023-12-14 15:58:36 +0700

    Dashboard warga

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,7 @@
 
   <!-- Font Awesome stylesheet -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  
   <!-- Template Main CSS File -->
   <link href="assets/css/home_style.css" rel="stylesheet">
 </head>
