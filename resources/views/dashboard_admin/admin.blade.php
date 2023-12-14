71cdf8b968cabf57f6984079cc1a41cdc15aeda1
IrfanD10 <irfandwianto110@gmail.com>
2023-12-14 08:46:03 +0700

    dashboard

---
+++ resources/views/dashboard_admin/admin.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master_dashboard')
+@section('content')
+        <div class="container-fluid" style="background: #f3f5fa">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Dashboard</div>
+                        <div class="card-body">
+                            <h4>Admin</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ use App\Http\Controllers\PesanController;
 //     return view('welcome');
 // });
 
+Route::get('admin', function () {
+    return view('dashboard_admin.admin');
+});
+
 Route::get('/', [HomeController::class, 'home'])->name('Home');
 Route::get('/login', [HomeController::class, 'login'])->name('Login');
 Route::post('/', [PesanController::class, 'store'])->name('pesan.store');
